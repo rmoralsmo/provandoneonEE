8e0187b913f0428f0a78708aa6b5b193b3e50ad0
rmoralesmo <rmorales@it-group.com.mx>
2017-03-27 12:29:29 -0600

    primer ee

--- src/NeonEE.java
+++ src/NeonEE.java
@@ -3,7 +3,7 @@ public class NeonEE {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("rene agrego este");
+		System.out.println("rene agrego este a neon");
 
 	}
 
