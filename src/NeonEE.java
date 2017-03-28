204860a642a2c5a2900f7327c5589f85074194f5
rmoralesmo <rmorales@it-group.com.mx>
2017-03-28 13:23:27 -0600

    brnches

--- src/NeonEE.java
+++ src/NeonEE.java
@@ -4,7 +4,7 @@ public class NeonEE {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("rene agrego este a neonr");
-		System.out.println("ae cambio a un nuevo branch1");
+		System.out.println("ae cambio a un nuevo branch12");
 
 	}
 
